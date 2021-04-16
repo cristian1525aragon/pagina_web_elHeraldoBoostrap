afef0b8b28d9d050e3b0c3a396e02706ecbc2dbe
Cristian Aragón <cristian2000aragon@gmail.com>
2021-04-15 22:59:33 -0500

    El Heraldo 3.0 React

---
+++ elheraldo/src/App.js
@@ -0,0 +1,20 @@
+
+import { Fragment } from 'react';
+import './App.css';
+import Header from './componets/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Fragment>
+       <Header titulo='EL HERALDO' />
+
+       </Fragment>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ elheraldo/src/componets/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header({titulo}) {
+    
+    
+    return (
+        <div class="page-header">  
+        
+        <p>{titulo}</p>
+
+        </div>
+    )
+}
+
+export default Header;
